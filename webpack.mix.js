3ef1ebfe6107bb5218035e4df18bedc5f7b920fc
Daniel Espinosa <daniel.espinosa@hippo.build>
2024-03-09 13:40:27 -0500

    Frontend: Tailwind CSS configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,4 +5,10 @@ const mix = require('laravel-mix');
  */
 mix.js('resources/js/app.js', 'public/js')
     .vue({ version: 2 })
-    .css('resources/css/app.css', 'public/css');
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ]);
+
+
